eb031a2e91080bf939a6e2d6b4f741303b3ddd9c
januwA <ajanuw1641845087@sina.com>
2020-01-11 20:31:51 +0800

    添加html-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,132 +1,127 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const CopyFilePlugin = require('webpack-copy-file-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyFilePlugin = require("webpack-copy-file-plugin");
 
 // 最小化生产
-const TerserJSPlugin = require('terser-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserJSPlugin = require("terser-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-const tsConfig = require('./tsconfig.json')
+const tsConfig = require("./tsconfig.json");
 
 function parseTsConfigPaths(tsConfig) {
-	const { paths, baseUrl } = tsConfig.compilerOptions
-	const alias = {};
-	if (paths) {
-		for (const aliasPath in paths) {
-			const key = aliasPath.replace(/\/\*$/, '')
-			const value = paths[aliasPath][0].replace(/\/\*$/, '');
-			alias[key] = path.resolve(__dirname, baseUrl, value);
-		}
-	}
-	return alias;
+  const { paths, baseUrl } = tsConfig.compilerOptions;
+  const alias = {};
+  if (paths) {
+    for (const aliasPath in paths) {
+      const key = aliasPath.replace(/\/\*$/, "");
+      const value = paths[aliasPath][0].replace(/\/\*$/, "");
+      alias[key] = path.resolve(__dirname, baseUrl, value);
+    }
+  }
+  return alias;
 }
 
 module.exports = {
-	mode: 'development', // production or development
-	entry: {
-		main: path.resolve(__dirname, 'src/index.ts')
-	},
-	devtool: 'inline-source-map',
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				use: 'ts-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: [
-					'style-loader',
-					{ loader: 'css-loader', options: { importLoaders: 1 } },
-					{
-						loader: 'postcss-loader',
-						options: {
-							ident: 'postcss',
-							sourceMap: true,
-							exec: true,
-							plugins: (loader) => [
-								require('postcss-import')({ root: loader.resourcePath }),
-								require('postcss-preset-env')(),
-								require('cssnano')()
-							]
-						}
-					},
-				]
-			},
-			{
-				test: /\.styl$/,
-				use: [
-					MiniCssExtractPlugin.loader,
-					{ loader: 'css-loader', options: { importLoaders: 1 } },
-					'stylus-loader',
-				],
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-					'file-loader',
-				],
-			},
-			{
-				test: /\.(woff|woff2|eot|ttf|otf)$/,
-				use: [
-					'file-loader',
-				],
-			},
-			{
-				test: /\.(csv|tsv)$/,
-				use: [
-					'csv-loader',
-				],
-			},
-			{
-				test: /\.xml$/,
-				use: [
-					'xml-loader',
-				],
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.tsx', '.ts', '.js'],
+  mode: "development", // production or development
+  entry: {
+    main: path.resolve(__dirname, "src/index.ts")
+  },
+  devtool: "inline-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          {
+            loader: "postcss-loader",
+            options: {
+              ident: "postcss",
+              sourceMap: true,
+              exec: true,
+              plugins: loader => [
+                require("postcss-import")({ root: loader.resourcePath }),
+                require("postcss-preset-env")(),
+                require("cssnano")()
+              ]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.styl$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "stylus-loader"
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ["file-loader"]
+      },
+      {
+        test: /\.(csv|tsv)$/,
+        use: ["csv-loader"]
+      },
+      {
+        test: /\.xml$/,
+        use: ["xml-loader"]
+      },
+      {
+        test: /\.html$/,
+        exclude: /node_modules/,
+        use: { loader: "html-loader" }
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
 
-		// 如果要配置路径别名，就在/tsconfig.json里面配置
-		alias: {
-			...parseTsConfigPaths(tsConfig)
-		}
-	},
-	optimization: {
-		minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})],
-	},
-	plugins: [
-		new CleanWebpackPlugin(),
-		new MiniCssExtractPlugin({
-			filename: '[name]-[hash].css',
-			chunkFilename: '[id].css',
-		}),
-		new HtmlWebpackPlugin({
-			template: 'src/index.html',
-		}),
-		new CopyFilePlugin([
-			'./README.md',
-		].map(f => path.resolve(__dirname, f)))
-	],
-	output: {
-		filename: '[name]-[hash].js',
-		path: path.resolve(__dirname, tsConfig.compilerOptions.outDir),
+    // 如果要配置路径别名，就在/tsconfig.json里面配置
+    alias: {
+      ...parseTsConfigPaths(tsConfig)
+    }
+  },
+  optimization: {
+    minimizer: [new TerserJSPlugin({}), new OptimizeCSSAssetsPlugin({})]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: "[name]-[hash].css",
+      chunkFilename: "[id].css"
+    }),
+    new HtmlWebpackPlugin({
+      template: "src/index.html"
+    }),
+    new CopyFilePlugin(["./README.md"].map(f => path.resolve(__dirname, f)))
+  ],
+  output: {
+    filename: "[name]-[hash].js",
+    path: path.resolve(__dirname, tsConfig.compilerOptions.outDir)
 
-		// 如果发布第三方包，可以启动下面这三个配置
-		// library: "packageName",
-		// libraryTarget: 'umd',
-		// globalObject: "this",
+    // 如果发布第三方包，可以启动下面这三个配置
+    // library: "packageName",
+    // libraryTarget: 'umd',
+    // globalObject: "this",
 
-		// <img src="./x.png" />
-		// publicPath: './',
+    // <img src="./x.png" />
+    // publicPath: './',
 
-		// <img src="./static/x.png" />
-		// publicPath: './static',
-	},
+    // <img src="./static/x.png" />
+    // publicPath: './static',
+  }
 };
