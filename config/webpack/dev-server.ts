a78d8e70e4ac4588eaf4e772bc9a6b26dd6e286f
januwA <ajanuw1995@gmail.com>
2021-09-06 16:14:35 +0800

    update packages and dev-server

--- config/webpack/dev-server.ts
+++ config/webpack/dev-server.ts
@@ -1,5 +1,5 @@
 import * as webpack from "webpack";
-import * as webpackDevServer from "webpack-dev-server";
+import * as DevServer from "webpack-dev-server";
 
 const isDevMode = process.env.NODE_ENV == "development";
 
@@ -9,34 +9,36 @@ import prodConfig from "./prod.config";
 const webpackConfig = isDevMode ? devConfig : prodConfig;
 
 // https://webpack.js.org/configuration/dev-server
-const options: webpackDevServer.Configuration = {
-  stats: "errors-only",
-  open: true, // 默认打开浏览器
+const options: DevServer.Configuration = {
+  open: false, // 默认打开浏览器
   host: "localhost",
   port: 4000, // 默认打开的端口
-  writeToDisk: true, // 结果输出到磁盘
   compress: true,
   historyApiFallback: true,
-  overlay: {
-    // warnings: true,
-    errors: true,
+  devMiddleware: {
+    stats: "errors-only",
+    writeToDisk: true, // 结果输出到磁盘
   },
-
+  client: {
+    overlay: {
+      // warnings: true,
+      errors: true,
+    },
+  },
   // 现在有个 /api/test 的请求会将请求代理到 http://localhost:3000/api/test
   // https://webpack.js.org/configuration/dev-server/#devserverproxy
   proxy: {
-    "/api": {
-      target: "http://localhost:3000",
+    "/users": {
+      target: "http://localhost:8000",
       secure: false,
       changeOrigin: true,
     },
   },
 };
 
-webpackDevServer.addDevServerEntrypoints(webpackConfig as any, options);
 const compiler: any = webpack(webpackConfig);
-const server = new webpackDevServer(compiler, options);
+const server = new DevServer(options, compiler);
 
-server.listen(options.port, options.host, () => {
-  console.log(`dev server listening ${options.host}:${options.port}`);
-});
+(async () => {
+  await server.start();
+})();
